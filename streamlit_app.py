1c38f36b139563170e6266eafe3e9eb733f55fd7
Sophy-Soun <108968775+Sophy-Soun@users.noreply.github.com>
2025-09-28 19:52:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ with st.expander("Data"):
   y
 with st.expander('Data Visulization'):
   # "bill_length_mm","bill_depth_mm","flipper_length_mm","body_mass_g","sex"
-  st.scater_chart(data = df, x ='bill_length_mm', y = 'body_mass_g', color = 'species')
+  st.scatter_chart(data = df, x ='bill_length_mm', y = 'body_mass_g', color = 'species')
