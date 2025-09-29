74c5d27b1f0d00282e608fcef66241c281972a0f
Sophy-Soun <108968775+Sophy-Soun@users.noreply.github.com>
2025-09-29 14:13:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,10 @@ with st.expander('Data Visulization'):
 
 with st.sidebar:
   st.header('Input Features')
+  island = st.selectbox('Island', ('Biscoe','Drean','Tergernson'))
+  bill_length_mm = st.slider('Bill Length (mm)',32.1,59.6, 43.9)
+  bill_dept_mm = st.slider('Bill Depth (mm)',13.1, 21.5, 17.2)
+  flipper_mm = st.slider('Fipper Length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.selectbox('Body Mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
+  
