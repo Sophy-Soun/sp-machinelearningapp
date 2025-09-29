90b31f4b7557306367593d7d2a3fb0aa7b56d2f4
Sophy-Soun <108968775+Sophy-Soun@users.noreply.github.com>
2025-09-29 15:35:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,11 +48,13 @@ input_row = df_penquins[:1]
 target_mapper = {'Adelie': 0,
                 'Chinstrap':1,
                 'Gento':2}
+
 def target_encode(val):
   return target_mapper[val]
   
 y = y_raw.apply(target_encode)
 y
+
 with st.expander('Input Features'):
   st.write('**Input Penquin**')
   input_df
